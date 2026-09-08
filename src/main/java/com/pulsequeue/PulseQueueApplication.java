0c65b893cce65c4d8a5851a3f0e366214daaa79e
Ayesha <ayesha19765@gmail.com>
2026-09-08 23:45:00 +0530

    Initialize PulseQueue Spring Boot project

---
+++ src/main/java/com/pulsequeue/PulseQueueApplication.java
@@ -0,0 +1,14 @@
+package com.pulsequeue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulseQueueApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/pulsequeue/PulseQueueApplicationTests.java
@@ -0,0 +1,14 @@
+package com.pulsequeue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
