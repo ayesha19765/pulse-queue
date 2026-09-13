2948690466a35dae33e60ba7fd7af7aea0bcb79b
Ayesha <ayesha19765@gmail.com>
2026-09-13 23:47:00 +0530

    Add job creation API

---
+++ src/main/java/com/pulsequeue/api/JobController.java
@@ -0,0 +1,35 @@
+package com.pulsequeue.api;
+
+import com.pulsequeue.application.JobService;
+import com.pulsequeue.domain.Job;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping
+    public ResponseEntity<JobResponse> createJob(@RequestBody CreateJobRequest request) {
+        Job job = jobService.createJob(request.getName(), request.getPriority());
+        JobResponse response = new JobResponse(
+                job.getId(),
+                job.getName(),
+                job.getPriority(),
+                job.getStatus(),
+                job.getCreatedAt()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
+
